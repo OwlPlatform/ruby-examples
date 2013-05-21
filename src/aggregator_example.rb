dfb83644652f2a784bf8d3f6708f1ba0a6cdb4cb
Ben Firner <bfirner@eden.rutgers.edu>
2013-05-21 00:28:52 -0400

    Adding example code.
      new file:   src/aggregator_example.rb
      new file:   src/world_model_example.rb

---
+++ src/aggregator_example.rb
@@ -0,0 +1,35 @@
+################################################################################
+#This program will connect to an aggregator and request all packets on phy 1.
+#Updates are requested for every 4000 milliseconds.
+################################################################################
+
+#Require rubygems for old (pre 1.9 versions of Ruby and Debian-based systems)
+require 'rubygems'
+require 'solver_aggregator'
+
+if (ARGV.length != 2)
+  puts "This program needs the ip address and client port of an aggregator to connect to!"
+  exit
+end
+
+aggip = ARGV[0]
+port = ARGV[1]
+
+sq = SolverAggregator.new(aggip, port)
+
+#Request packets from physical layer 1, don't specify a transmitter ID or
+#mask, and request packets every 4000 milliseconds
+#Phy 1 is for pips. Phy 0 is for all devices on every phy.
+sq.sendSubscription([AggrRule.new(1, [], 4000)])
+
+while (sq.handleMessage) do
+  if (sq.available_packets.length != 0) then
+    puts "Processing some packets!"
+    for packet in sq.available_packets do
+      puts packet
+    end
+  end
+end
+
+puts "connection closed"
+
---
+++ src/world_model_example.rb
@@ -0,0 +1,56 @@
+################################################################################
+#This program will connect to a world model at a user-provided address and port
+#and request a few pieces of information, which it will then print.
+################################################################################
+
+#Require rubygems for old (pre 1.9 versions of Ruby and Debian-based systems)
+require 'rubygems'
+require 'client_world_connection.rb'
+require 'wm_data.rb'
+require 'buffer_manip.rb'
+
+if (ARGV.length != 2)
+  puts "This program needs the ip address and client port of a world model to connect to!"
+  exit
+end
+
+wmip = ARGV[0]
+port = ARGV[1]
+
+#Connect to the world model as a client
+cwm = ClientWorldConnection.new(wmip, port)
+
+#Search just for all names by providing the .* pattern.
+result = cwm.URISearch('.*')
+puts "Found uris #{result.get()}"
+
+#Search for all uris and get all of their attributes
+#There two arguments to the snapshotRequest. The first is a REGEX pattern of the
+#names of objects to search for. The second is a list of patterns that match
+#attributes (in this case we just give one pattern that matches all attributes).
+puts "Searching for all URIs and attributes"
+result = cwm.snapshotRequest('.*', ['.*']).get()
+result.each_pair {|uri, attributes|
+  puts "Found uri \"#{uri}\" with attributes:"
+  attributes.each {|attr|
+    puts "\t#{attr.name} is #{attr.data.unpack('G')}"
+  }
+}
+
+#Get the locations of mugs with updates every second. The third argument to
+#the stream request is the request update interval in milliseconds. The first
+#two arguments are the same as the snapshot request.
+#This is a streaming requests so it will persist until we cancel it
+#Cancelling would look like this: mug_request.cancel
+mug_request = cwm.streamRequest(".*\.mug\..*", ['location\..offset'], 1000)
+while (cwm.connected and not mug_request.isComplete())
+  result = mug_request.next()
+  result.each_pair {|uri, attributes|
+    puts "Found mug \"#{uri}\""
+    puts "Location updates are:"
+    attributes.each {|attr|
+      puts "\t#{attr.name} is #{attr.data.unpack('G')}"
+    }
+  }
+end
+
