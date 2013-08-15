55df7bf7d809687bae3a113f41941fd818dd86f4
Ben Firner <bfirner@eden.rutgers.edu>
2013-08-15 06:19:06 -0400

    Adding range example.
    	new file:   range_example.rb

---
+++ src/range_example.rb
@@ -0,0 +1,43 @@
+################################################################################
+#This program will connect to a world model at a user-provided address and port
+#and request a few pieces of information, which it will then print.
+################################################################################
+
+#Require rubygems for old (pre 1.9 versions of Ruby and Debian-based systems)
+require 'rubygems'
+require 'client_world_connection.rb'
+require 'wm_data.rb'
+require 'buffer_manip.rb'
+
+if (ARGV.length != 2)
+  puts "This program needs the ip address and client port of a world model to connect to!"
+  exit
+end
+
+wmip = ARGV[0]
+port = ARGV[1]
+
+#Connect to the world model as a client
+cwm = ClientWorldConnection.new(wmip, port)
+
+#Search for all uris and get all of their attributes
+#There two arguments to the snapshotRequest. The first is a REGEX pattern of the
+#names of objects to search for. The second is a list of patterns that match
+#attributes (in this case we just give one pattern that matches all attributes).
+puts "Searching for all URIs and attributes"
+current_time = getMsecTime()
+query =  cwm.rangeRequest('.*', ['closed'], 0, current_time)
+while (not query.isComplete())
+	result = query.next()
+	result.each_pair {|uri, attributes|
+		puts "Found uri \"#{uri}\" with attributes:"
+		attributes.each {|attr|
+			puts attr
+			#Example of decoding some data
+			if (attr.name == "temperature.celcius")
+				puts "temperature in celsius is #{attr.data.unpack('G')}"
+			end
+		}
+	}
+end
+
