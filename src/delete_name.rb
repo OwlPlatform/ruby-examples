eb6206ad24e679b3eea4213822f521084c2696aa
Ben Firner <bfirner@eden.rutgers.edu>
2013-08-24 09:02:43 -0400

    Adding an example to delete something from the world model.
    	new file:   delete_name.rb

---
+++ src/delete_name.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'libowl/solver_world_model.rb'
+
+if (ARGV.length != 3)
+  puts "The first two arguments of this program are the ip address and"
+  puts "solver port of a world model to connect to! The next argument is"
+	puts "the name of an object to delete from the world model."
+  exit
+end
+
+wmip = ARGV[0]
+port = ARGV[1]
+target = ARGV[2]
+
+wm = SolverWorldModel.new(wmip, port, 'delete name script')
+
+#Delete the object with the given name
+wm.deleteURI(target)
